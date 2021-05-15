68b826af85987696001ce3c6d68dc36a05395aad
김승현 <dnfheh@naver.com>
2021-05-15 23:39:24 +0900

    [Collection]

---
+++ src/PC_prac/Collection.java
@@ -0,0 +1,33 @@
+package PC_prac;
+
+public class Collection {
+
+	
+	
+	/*
+	 * JAVA 의 대표 Collection에는 어떤것들이 있나요?
+	 * 
+	 * 자바의 대표 Collection에는 List,Map,Set,Stack,Queue와 같은 것들이 있습니다. 이 추상화된 Collection
+	 * 인터페이스 아래, 특정한 기법으로 구현된 자료구조가 들어갑니다 예를들어, List라는 인터페이스에는 구현방법에 따라 ArrayList가
+	 * 들어갈수도 , LinkedList가 들어갈 수도 있습니다.
+	 * 
+	 * 
+	 * 2.List에 대해 설명해주세요. 리스트는 배열과 비슷한 자바의 자료형으로 배열보다 편리한 기능을 많이 가지고 있습니다. List에는
+	 * 대표적으로 ArrayList와 LinkedList가 있습니다.
+	 * 
+	 * ArrayList는 자바의 Vector를 개선한, 배열로 구현된 List입니다. 배열과 같은 자료구조이기 때문에, 리스트의 연산 수행시간
+	 * 속도는 배열과 같습니다.
+	 * 
+	 * LinkedList는 다음 노드의 주소를 기억하고 있는 List로 배열에 비해 삽입과 삭제가 간단하다는 장점이 있습니다. 하지만 탐색의
+	 * 경우에는 첫번째 노드부터 탐색햏 나가야 하기 때문에 느리다는 단점이 있습니다.
+	 * 
+	 * 
+	 * 3. Map에 대해 설명해 주세요
+	 * 
+	 * Map에는 HashMap, TreeMap, linkedHashMap이 있습니다. 그중 HashMap은 가장 일반적으로 사용하는 Map입니다
+	 * HashTable을 사용하며 key값에 해시함수를 적용하여 나온 index에
+	 */
+
+
+
+}
