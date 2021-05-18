2edb4ef14ac906787b668490698f0130c698a29a
김승현 <dnfheh@naver.com>
2021-05-19 00:10:58 +0900

    [jdbc]

---
+++ src/PC_prac/JdbcTemplate.java
@@ -0,0 +1,23 @@
+package PC_prac;
+
+public class JdbcTemplate {
+
+	
+	
+	/*
+	 * JDBC의 단점을 보완한 JdbcTemplate JDBC 드라이버 로딩 -> DB 연결 -> SQL 작성 및 전송 -> 자원해제
+	 * 
+	 * JdbcTemplate 드라이버로딩,연결,자원해제 -> SQL작성 및 전송
+	 */
+	
+	
+	/*
+	 * DataSource 클래스 
+	 * 스프링
+	 * org.springframework.jdbc.datasource.DriverManagerDataSource
+	 * c3p0 
+	 * com.mchange.v2.c3p0. DriverManagerDataSource  
+	 * 데이터베이스 연결과 관련된 정보를 가지고
+	 * 있는 DataSource는 스프링 또는 c3p0에 제공하는 클래스를 이용할 수 있다
+	 */
+}
