6765b3de1c49aec42135add7e63f73beccf5d0f9
김승현 <dnfheh@naver.com>
2021-05-22 05:35:45 +0900

    [MVC추가]

--- src/PC_prac/Spring.java
+++ src/PC_prac/Spring.java
@@ -36,5 +36,27 @@ public class Spring {
 	 * @Inject의 경우 required 속성을 지원하지 않는다.
 	 */
 	
+	/*
+	 * 브라우저 -> 컨트롤러 -> 서비스 -> DAO -> model <-> model -> 데이터베이스
+	 * 
+	 * 
+	 * Model1 방식은 모든코드를 하나에 때려박아서 빠른 구현가능 But 유지보수가 힘듬
+	 * 
+	 * Model2 MVC 패턴으로 각자 역할을 나누어 쉬운 유지보수 가능
+	 */
+	
+	/*
+	 * DispatcherServlet -> 요청 ex)날씨 요청 -> (1)HandlerMapping = 수 많은 컨트롤러 중에 가장 적합한
+	 * 녀석을 찾아줘 -> 다시 HandlerMapping 이 DispatcherServlet 으로 와서 -> (2)
+	 * DispatcherServlet -> HandlerAdater 한테 또 요청 컨트롤러 객체가 있으면 많은 메서드가 있을 수 있지 이런
+	 * 메서드 중에 사용자가 요청한걸 처리할 수 있는 가장 적합한 메서드 찾은다음 Model 이라는 데이터로 가져와요! 컨트롤러를 거쳐서 다시
+	 * 돌아오면 DispatcherServlet은 View Resolver를 또 찾아유 요 컨트롤러 뒤에는 Service,Dao,DB가 있겠쥬
+	 * 이것들을 다 묶어서 Controller라고 생각하고 적는중이다 View Resolver는 이 컨트롤러 부터 모델과 데이터가 왔는디 이
+	 * View에 해당하는 가장 적합한 JSP를 찾아주세요! 그럼 가장 적합한 JSP를 클라이언트한테 응답해준다! 이야!
+	 */
+	 
+	
+
+	
 	
 }
