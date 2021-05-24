8a64da802603420191dbaf09b44e044d03e7f368
김승현 <dnfheh@naver.com>
2021-05-24 09:39:04 +0900

    [SQL developer]

---
+++ src/Spring/SqlDeveloper.java
@@ -0,0 +1,20 @@
+package Spring;
+
+public class SqlDeveloper {
+
+	
+//	간단하게
+//	1.오라클설치
+//	2. 계정생성 
+//	-> cmd -> sqlplus -> Enter user-name pw , 오라클 버전확인
+//	create user scott identified by tiger;
+//    User created.
+//    grant connect. resource to scott;
+//    Grant succeeded
+//    exit -> name scott -> show user "SCOTT"
+//    3.SQL developer 접속
+//    접속 이름 scottcon
+//    사용자 이름 scott
+//    새접속
+	
+}
