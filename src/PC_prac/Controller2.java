a2c65fa5d57c125c56cbf314d7fe03d1a8d83d42
김승현 <dnfheh@naver.com>
2021-05-25 11:23:17 +0900

    [Redirect]

---
+++ src/PC_prac/Controller2.java
@@ -0,0 +1,13 @@
+package PC_prac;
+
+public class Controller2 {
+
+	
+//	phone 번호가 집전화, 휴대전화 이렇게 두개 받아야 하는 경우
+//	String memberPhone 으로 중첩적용이 안되니까 각자 이름을 바꿔주거나
+//	List<MemberPhone> memberPhones 리스트형으로 받아줄 수 있다.
+//	MemberPhone vo를 생성해 
+//	jsp에서 번호를 받을 때 memberPhones[0].memberPhone
+//			,memberPhones[1].memberPhone 
+//			배열로 받는다.
+}
