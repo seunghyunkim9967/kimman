6d5767b29d70b8d59422633121010994957b90dc
김승현 <dnfheh@naver.com>
2021-06-01 12:20:21 +0900

    [JAVA]

---
+++ src/prac/JAVA.java
@@ -0,0 +1,87 @@
+package prac;
+
+public class JAVA {
+
+	
+	/*
+	 * api 주요 기능 같은것을 미리 구현한 라이브러리 집합
+	 * 
+	 * 
+	 * IDE 통합 개발환경
+	 * 
+	 * JDK 자바응용 개발 환경
+	 * 
+	 * JRE 실행환경 , 그안에 JVM이 포함되고 라이브러리랑 API가 포함되어있는것
+	 * 
+	 * 자바의 특성 플랫폼에 독립적, 객체지향
+	 * 
+	 * 캡슐화 상속성 다형성
+	 * 
+	 * 자료형
+	 * 
+	 * 문자 char 정수 int byte short long 실수 double float 논리 boolean 기본 자료형들은 운영체제에 다르지
+	 * 않게 모두 똑같음
+	 * 
+	 * String은 클래스로 들어간다.
+	 * 
+	 * 
+	 * 
+	 * Var = 다양하다는 뜻 숫자가 오면 int 문자가오면 char
+	 * 
+	 * 
+	 * Scanner 입력받을때 스캐너객체를 생성해서 사용해야함
+	 * 
+	 * 
+	 * 생성자란 객체를 초기화 하기위해 생성되는 메서드
+	 * 
+	 * 
+	 * 
+	 * for(i=1 , i<=10; i++,hap+=i){ syso ,.... i hap }
+	 * 
+	 * i hap 1 1
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 10 54
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * public class circle{
+	 * 
+	 * private int 반지름;
+	 * 
+	 * public circle( ) { this.반지름 = 100; }
+	 * 
+	 * public circle(int a) { } this.반지름 = a; }
+	 * 
+	 * 
+	 * main(){ }
+	 * 
+	 * circle pizza = new circle(150);
+	 * 
+	 * 
+	 * 
+	 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
